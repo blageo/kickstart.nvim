2ebb7ac5b52071829fb8e7fb55859e0d205434a2
blageo <blageo@proton.me>
2024-06-21 13:52:15 -0400

    Tmux integration

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,11 +1,1 @@
-return {
-  'catppuccin/nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-  end,
-  config = function()
-    vim.cmd.colorscheme 'catppuccin'
-  end,
-}
+../../../../../dotfiles/.config/nvim/lua/custom/plugins/catppuccin.lua
